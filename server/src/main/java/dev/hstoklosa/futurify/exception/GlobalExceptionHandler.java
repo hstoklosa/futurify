156638db511683d68ecfdada6b13718026c8f053
hstoklosa <hubert.stoklosa23@gmail.com>
2024-08-05 02:08:57 +0100

    minor changes to the GlobalExceptionHandler

--- server/src/main/java/dev/hstoklosa/futurify/exception/GlobalExceptionHandler.java
+++ server/src/main/java/dev/hstoklosa/futurify/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package dev.hstoklosa.futurify.exception;
 
 import dev.hstoklosa.futurify.payload.response.ApiErrorResponse;
 import dev.hstoklosa.futurify.payload.response.GenericApiResponse;
+import jakarta.mail.MessagingException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +18,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<GenericApiResponse<?>> handleException(
-        ResourceNotFoundException e,
+        ResourceNotFoundException exp,
         HttpServletRequest request
     ) {
         return ResponseEntity
@@ -25,7 +26,7 @@ public class GlobalExceptionHandler {
                 .body(GenericApiResponse.error(
                         ApiErrorResponse.builder()
                                 .path(request.getRequestURI())
-                                .message(e.getMessage())
+                                .message(exp.getMessage())
                                 .timestamp(LocalDateTime.now())
                                 .build()
                         )
@@ -34,7 +35,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(DuplicateResourceException.class)
     public ResponseEntity<GenericApiResponse<?>> handleException(
-            DuplicateResourceException e,
+            DuplicateResourceException exp,
             HttpServletRequest request
     ) {
         return ResponseEntity
@@ -42,7 +43,7 @@ public class GlobalExceptionHandler {
                 .body(GenericApiResponse.error(
                         ApiErrorResponse.builder()
                                 .path(request.getRequestURI())
-                                .message(e.getMessage())
+                                .message(exp.getMessage())
                                 .timestamp(LocalDateTime.now())
                                 .build()
                         )
@@ -51,7 +52,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(InsufficientAuthenticationException.class)
     public ResponseEntity<GenericApiResponse<?>> handleException(
-        InsufficientAuthenticationException e,
+        InsufficientAuthenticationException exp,
         HttpServletRequest request
     ) {
         return ResponseEntity
@@ -68,7 +69,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<GenericApiResponse<?>> handleException(
-        BadCredentialsException e,
+        BadCredentialsException exp,
         HttpServletRequest request
     ) {
         return ResponseEntity
@@ -83,9 +84,25 @@ public class GlobalExceptionHandler {
                 );
     }
 
+    @ExceptionHandler(MessagingException.class)
+    public ResponseEntity<GenericApiResponse<?>> handleException(
+        MessagingException exp,
+        HttpServletRequest request
+    ) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(GenericApiResponse.error(
+                        ApiErrorResponse.builder()
+                                .path(request.getRequestURI())
+                                .message(exp.getMessage())
+                                .timestamp(LocalDateTime.now())
+                                .build()
+                ));
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<GenericApiResponse<?>> handleException(
-        Exception e,
+        Exception exp,
         HttpServletRequest request
     ) {
         return ResponseEntity
