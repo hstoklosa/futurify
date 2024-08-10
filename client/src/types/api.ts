80f745baf54e65c93af5cd0d700011d12f3d6209
hstoklosa <hubert.stoklosa23@gmail.com>
2024-08-10 02:16:38 +0100

    add User type to use when fetching from an api

---
+++ client/src/types/api.ts
@@ -0,0 +1,15 @@
+export type BaseEntity = {
+    id: string;
+};
+
+export type Entity<T> = {
+    [K in keyof T]: T[K];
+} & BaseEntity;
+
+export type User = Entity<{
+    firstName: string;
+    lastName: string;
+    email: string;
+    enabled: boolean;
+    role: "admin" | "user";
+}>;
