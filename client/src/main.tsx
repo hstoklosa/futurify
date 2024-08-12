9cf3382d14375adef20e17a33a2101b4f32d16b9
hstoklosa <hubert.stoklosa23@gmail.com>
2024-08-12 20:48:29 +0100

    install @tanstack/react-query-devtools for development purposes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { queryClient } from "@lib/react-query";
 import router from "./routes";
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
             <RouterProvider router={router} />
+            <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
     </React.StrictMode>
 );
