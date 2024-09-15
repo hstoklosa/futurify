5713f7b5689051b3bd1da4f88e117b5e2db63967
hstoklosa <hubert.stoklosa23@gmail.com>
2024-09-15 18:32:40 +0100

    remove unnecessary code from LogoutHandler

--- server/src/main/java/dev/hstoklosa/futurify/service/LogoutService.java
+++ server/src/main/java/dev/hstoklosa/futurify/service/LogoutService.java
@@ -1,8 +1,5 @@
 package dev.hstoklosa.futurify.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import dev.hstoklosa.futurify.common.api.ApiResponse;
-import dev.hstoklosa.futurify.common.api.ResponseFactory;
 import dev.hstoklosa.futurify.common.util.CookieUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -15,34 +12,18 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-
 @Service
 @RequiredArgsConstructor
 public class LogoutService implements LogoutHandler {
-    private final CookieUtil cookieUtil;
-    private final ObjectMapper objectMapper;
-
     @Override
     public void logout(
         HttpServletRequest request,
         HttpServletResponse response,
         Authentication authentication
     ) {
-        ResponseCookie accessTokenCookie = CookieUtil.getCleanAccessTokenCookie();
-        ResponseCookie refreshTokenCookie = CookieUtil.getCleanRefreshTokenCookie();
-
-        response.addHeader(HttpHeaders.SET_COOKIE, accessTokenCookie.toString());
-        response.addHeader(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString());
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-
+        response.addHeader(HttpHeaders.SET_COOKIE, CookieUtil.getCleanAccessTokenCookie().toString());
+        response.addHeader(HttpHeaders.SET_COOKIE, CookieUtil.getCleanRefreshTokenCookie().toString());
         SecurityContextHolder.clearContext();
-
-        try {
-            ApiResponse<Void> apiResponse = ResponseFactory.success();
-            objectMapper.writeValue(response.getWriter(), apiResponse);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
