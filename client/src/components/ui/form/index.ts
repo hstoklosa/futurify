30aa9c2bf8d1dc3b57e563d6e6225431801d900e
hstoklosa <hubert.stoklosa23@gmail.com>
2024-10-03 09:53:42 +0100

    add exports to index file in ui/form directory

--- client/src/components/ui/form/index.ts
+++ client/src/components/ui/form/index.ts
@@ -1,2 +1,4 @@
 export { default as Input } from "./Input";
 export { default as Form } from "./Form";
+export { default as Select } from "./select";
+export { default as TextEditor } from "./text-editor";
