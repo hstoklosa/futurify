c40ce0fa0fdb6f7e449406c42e29ccbef9d2b7b8
hstoklosa <hubert.stoklosa23@gmail.com>
2024-10-09 17:41:28 +0100

    improve dry code when handling disabled tooltips on the sidebar

--- client/src/components/layout/AppLayout.tsx
+++ client/src/components/layout/AppLayout.tsx
@@ -11,18 +11,19 @@ import {
   LuChevronsUpDown,
   LuHelpCircle,
 } from "react-icons/lu";
-import icon from "../../assets/icon.png";
+
+import icon from "@assets/icon.png";
 
 import {
   DropdownMenu,
   DropdownMenuTrigger,
   DropdownMenuContent,
   DropdownMenuItem,
-} from "@components/ui/dropdown";
-import { Button } from "@components/ui/button";
-import { Tooltip, TooltipTrigger, TooltipContent } from "@components/ui/tooltip";
-import { cn } from "@utils/cn";
+} from "../ui/dropdown";
+import { Button } from "../ui/button";
+import { Tooltip, TooltipTrigger, TooltipContent } from "../ui/tooltip";
 import { PathConstants } from "@utils/constants";
+import { cn } from "@utils/cn";
 
 import { useUser } from "@features/auth/api/getUser";
 import { useLogout } from "@features/auth/api/logout";
@@ -41,6 +42,11 @@ const navigation: SidebarItem[] = [
   { name: "Home", to: PathConstants.HOME, Icon: LuHome, disabled: false },
   { name: "Contacts", to: "/contacts", Icon: LuContact2, disabled: true },
   { name: "Documents", to: "/documents", Icon: LuFile, disabled: true },
+];
+
+const buttons: Omit<SidebarItem, "to">[] = [
+  { name: "Help", Icon: LuHelpCircle, disabled: true },
+  { name: "Settings", Icon: LuSettings, disabled: true },
 ];
 
 const SidebarLink = ({ to, Icon, name, disabled }: SidebarItem) => {
@@ -71,23 +77,21 @@ const SidebarLink = ({ to, Icon, name, disabled }: SidebarItem) => {
     </NavLink>
   );
 
-  return (
-    <div>
-      {disabled ? (
-        <Tooltip>
-          <TooltipTrigger className="">{link}</TooltipTrigger>
-          <TooltipContent
-            side="right"
-            sideOffset={15}
-          >
-            {disabled && <span>Coming Soon</span>}
-          </TooltipContent>
-        </Tooltip>
-      ) : (
-        link
-      )}
-    </div>
-  );
+  if (disabled) {
+    return (
+      <Tooltip>
+        <TooltipTrigger>{link}</TooltipTrigger>
+        <TooltipContent
+          side="right"
+          sideOffset={15}
+        >
+          {disabled && <span>Coming soon...</span>}
+        </TooltipContent>
+      </Tooltip>
+    );
+  }
+
+  return link;
 };
 
 const SidebarBoardLink = ({ id, name }: { id: number; name: string }) => (
@@ -106,7 +110,7 @@ const SidebarBoardLink = ({ id, name }: { id: number; name: string }) => (
       {name}
     </span>
 
-    <div className="flex grow justify-end">
+    <div className="flex grow justify-end ml-[2.5px]">
       <ArchiveBoardDialog id={id.toString()} />
     </div>
   </NavLink>
@@ -131,21 +135,24 @@ const AppLayout = () => {
     <div className="flex w-full h-full bg-background overflow-x-auto">
       <aside
         className={cn(
-          "fixed inset-y-0 left-0 z-30 max-w-[215px] w-full bg-background transform transition-transform duration-300 ease-in-out md:relative md:translate-x-0 border-border border-r-[1px]",
+          "fixed inset-y-0 left-0 z-30 max-w-[215px] w-full bg-background border-border border-r-[1px]",
+          "transform transition-transform duration-300 ease-in-out",
+          "md:relative md:translate-x-0",
           sidebarOpen ? "translate-x-0" : "-translate-x-full"
         )}
       >
         <div className="flex flex-col h-full">
-          <div className="px-2 pt-3 mb-2">
+          <div className="px-2 pt-3 mb-1">
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
-                <button className="flex items-center w-full text-left h-10 px-3 py-1 border-border border-[1px] rounded-md focus:outline-none mt-auto">
+                <button className="flex items-center w-full text-left h-10 px-3 py-1 border-border shadow-4xl border-[1px] rounded-md focus:outline-none mt-auto">
                   <img
                     src={icon}
                     alt="icon"
                     className="w-5 h-5 mr-2"
                   />
-                  <span className="text-foreground/80 text-sm font-semibold truncate max-w-[80px]">
+
+                  <span className="text-foreground/80 text-sm font-semibold truncate max-w-[120px]">
                     {currentUser!.data.firstName}
                   </span>
 
@@ -206,7 +213,7 @@ const AppLayout = () => {
                       />
                     ))}
 
-                  {!boards?.length && !boardsQuery.isLoading && (
+                  {!boardsQuery.isLoading && !boards?.length && (
                     <p className="text-foreground/30 text-sm px-4 py-1">
                       No Boards Created
                     </p>
@@ -216,14 +223,18 @@ const AppLayout = () => {
             </div>
           </nav>
 
-          {/* User Options */}
-          <div className="px-2 pb-3 [&>*]:w-full [&>*]:min-h-8">
-            <button className="flex items-center px-4 rounded-md hover:bg-primary/5 text-foreground/80 text-sm font-semibold truncate disabled cursor-not-allowed">
-              <LuHelpCircle className="w-[17px] h-[17px] mr-2" /> Support
-            </button>
-            <button className="flex items-center px-4 rounded-md hover:bg-primary/5 text-foreground/80 text-sm font-semibold truncate disabled cursor-not-allowed">
-              <LuSettings className="w-[17px] h-[17px] mr-2" /> Settings
-            </button>
+          {/* User Buttons */}
+          <div className="px-2 pb-1.5">
+            {buttons.map(({ name, Icon, disabled }) => (
+              <button
+                key={name}
+                disabled={disabled}
+                className="flex items-center w-full min-h-8 px-4 rounded-md hover:bg-primary/5 text-foreground/80 text-sm font-semibold truncate disabled cursor-not-allowed"
+              >
+                <Icon className="w-[17px] h-[17px] mr-2" />
+                {name}
+              </button>
+            ))}
           </div>
         </div>
       </aside>
@@ -234,7 +245,7 @@ const AppLayout = () => {
       {/* Mobile Sidebar Toggle */}
       <button
         onClick={() => setSidebarOpen(!sidebarOpen)}
-        className="fixed left-5 bottom-5 bg-background flex justify-center items-center p-1 h-[40px] w-[40px] border-border border-[1px] text-white rounded-lg shadow-lg md:hidden"
+        className="fixed left-5 bottom-5 flex justify-center items-center bg-background p-1 h-[40px] w-[40px] border-border border-[1px] text-white rounded-lg shadow-lg md:hidden"
       >
         <LuAlignJustify className="stroke-black stroke-width-[0.1px] w-[22px] h-[22px]" />
       </button>
