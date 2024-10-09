7abbce1ac26a8f988cae201a497fd039b223486c
hstoklosa <hubert.stoklosa23@gmail.com>
2024-10-09 18:19:29 +0100

    integrate custom scrollbar into radix ui dialogs

--- client/src/components/ui/dialog/Dialog.tsx
+++ client/src/components/ui/dialog/Dialog.tsx
@@ -3,6 +3,8 @@ import * as DialogPrimitive from "@radix-ui/react-dialog";
 
 import { cn } from "@utils/cn";
 
+import { ScrollArea } from "../scroll-area";
+
 const Dialog = DialogPrimitive.Root;
 const DialogTrigger = DialogPrimitive.Trigger;
 const DialogPortal = DialogPrimitive.Portal;
@@ -32,14 +34,14 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed top-[50%] left-[50%] z-50 w-[350px] max-h-[85vh] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-background duration-200 overflow-y-auto",
+        "fixed top-[50%] left-[50%] z-50 w-[350px] max-h-[85vh] h-[50vh] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-background duration-200",
         "data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95 data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]",
         "data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%]",
         className
       )}
       {...props}
     >
-      {children}
+      <ScrollArea className="h-full">{children}</ScrollArea>
     </DialogPrimitive.Content>
   </DialogPortal>
 ));
@@ -64,7 +66,7 @@ const DialogHeader = ({
 }: React.HTMLAttributes<HTMLDivElement>) => (
   <div
     className={cn(
-      "flex justify-center items-center h-[40px] border-[1px] border-b border-border",
+      "flex justify-center items-center h-[40px] border-b border-border",
       className
     )}
     {...props}
