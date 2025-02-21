ef0820a0e22c2eb88e3e84f5fdae39d8dadc6fe8
hstoklosa <hubert.stoklosa23@gmail.com>
2025-02-21 08:07:46 +0000

    chore: update vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { configDefaults } from 'vitest/config';
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -8,6 +9,19 @@ export default defineConfig({
     server: {
         watch: {
             usePolling: true,
+        },
+    },
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: ['./src/test/setup.ts'],
+        exclude: [...configDefaults.exclude, 'e2e/*'],
+        coverage: {
+            reporter: ['text', 'json', 'html'],
+            exclude: [
+                'node_modules/',
+                'src/test/setup.ts',
+            ],
         },
     },
 });
